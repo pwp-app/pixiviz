dc433607b2bb1b82aca910a1eb052aca6a38b5da
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-04-25 00:50:16 +0800

    fix: rank waterfall responsive initial value

--- src/views/Rank.vue
+++ src/views/Rank.vue
@@ -137,7 +137,7 @@ export default {
             waterfallIdentifier: Math.round(Math.random() * 100),
             screenWidth: document.documentElement.clientWidth,
             cardWidth: this.getCardWidth(document.documentElement.clientWidth),
-            waterfallResponsive: true,
+            waterfallResponsive: this.screenWidth > 767,
             scrollTop: 0,
         };
     },
