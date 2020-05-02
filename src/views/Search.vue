9559fb5ff8b90a45b3cb2192ead421b131d8cbe2
BackRunner <12205593+backrunner@users.noreply.github.com>
2020-05-02 22:55:00 +0800

    fix: search waterfall width

--- src/views/Search.vue
+++ src/views/Search.vue
@@ -97,7 +97,7 @@ export default {
             // Misc
             screenWidth: document.documentElement.clientWidth,
             cardWidth: this.getCardWidth(document.documentElement.clientWidth),
-            waterfallResponsive: document.documentElement.clientWidth > 767 && window.orientation === 0,
+            waterfallResponsive: document.documentElement.clientWidth > 767,
             scrollTop: 0,
             // style
             iPadStyle: /iPad/i.test(navigator.userAgent)
