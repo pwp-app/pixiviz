f5db36bafbc85d20b7be6915bea384b25e2e8d26
BackRunner <dev@backrunner.top>
2020-11-16 16:26:51 +0800

    refactor: format

--- src/App.vue
+++ src/App.vue
@@ -1,12 +1,12 @@
 <template>
-    <div id="app">
-        <transition mode="out-in">
-            <keep-alive v-if="$route.meta.keepAlive">
-                <router-view></router-view>
-            </keep-alive>
-            <router-view v-else></router-view>
-        </transition>
-    </div>
+  <div id="app">
+    <transition mode="out-in">
+      <keep-alive v-if="$route.meta.keepAlive">
+        <router-view></router-view>
+      </keep-alive>
+      <router-view v-else></router-view>
+    </transition>
+  </div>
 </template>
 
 <script>
@@ -14,89 +14,89 @@ import dayjs from 'dayjs';
 import CONFIG from './config.json';
 
 export default {
-    name: "app",
-    beforeCreate() {
-        // 全局构建
-        if (!window.pixiviz) {
-            window.pixiviz = {};
-        }
-        // 设置全局map
-        if (!window.pixiviz.infoMap) {
-            window.pixiviz.infoMap = {};
-        }
-        if (!window.pixiviz.artistMap) {
-            window.pixiviz.artistMap = {};
-        }
-        // 初始化图片负载表
-        if (!window.pixiviz.loadMap) {
-            const loadMap = JSON.parse(window.localStorage.getItem('loadmap'));
-            const loadMapTime = window.localStorage.getItem('loadmap-save-time');
-            if (!loadMap || !loadMapTime) {
-                window.pixiviz.loadMap = {};
-                // loadMap有效期3小时
-            } else if (loadMapTime && (new Date().valueOf() - loadMapTime) / 1000 > 10800) {
-                window.pixiviz.loadMap = {};
-            } else if (loadMap) {
-                window.pixiviz.loadMap = loadMap;
-            }
-        }
-        // 异步获取分流配置
-        this.axios.get('https://config.backrunner.top/pixiviz/proxy-config.json', {
-            withCredentials: false,
-        }).then((res) => {
-            if (res.data) {
-                let index = 0;
-                window.pixiviz.hostMap = {};
-                Object.keys(res.data).forEach((key) => {
-                    window.pixiviz.hostMap[index] = key;
-                    window.pixiviz.hostMap[key] = index;
-                    index++;
-                });
-                window.pixiviz.proxyMap = res.data;
-            }
-        }, () => {
-            console.log('Cannot fetch proxy config.');
-        });
-    },
-    created() {
-        // 检测Safari
-        window.isSafari = /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
-    },
-    mounted() {
-        // add save loadmap listener
-        window.addEventListener('beforeunload', this.saveLoadMap);
-    },
-    destroyed() {
-        window.removeEventListener('beforeunload', this.saveLoadMap);
-    },
-    methods: {
-        saveLoadMap() {
-            if (window.pixiviz.loadMap && Object.keys(window.pixiviz.loadMap).length > 0) {
-                window.localStorage.setItem('loadmap', JSON.stringify(window.pixiviz.loadMap));
-                window.localStorage.setItem('loadmap-save-time', new Date().valueOf());
-            }
-        }
+  name: "app",
+  beforeCreate() {
+    // 全局构建
+    if (!window.pixiviz) {
+      window.pixiviz = {};
     }
+    // 设置全局map
+    if (!window.pixiviz.infoMap) {
+      window.pixiviz.infoMap = {};
+    }
+    if (!window.pixiviz.artistMap) {
+      window.pixiviz.artistMap = {};
+    }
+    // 初始化图片负载表
+    if (!window.pixiviz.loadMap) {
+      const loadMap = JSON.parse(window.localStorage.getItem('loadmap'));
+      const loadMapTime = window.localStorage.getItem('loadmap-save-time');
+      if (!loadMap || !loadMapTime) {
+        window.pixiviz.loadMap = {};
+        // loadMap有效期3小时
+      } else if (loadMapTime && (new Date().valueOf() - loadMapTime) / 1000 > 10800) {
+        window.pixiviz.loadMap = {};
+      } else if (loadMap) {
+        window.pixiviz.loadMap = loadMap;
+      }
+    }
+    // 异步获取分流配置
+    this.axios.get('https://config.backrunner.top/pixiviz/proxy-config.json', {
+      withCredentials: false,
+    }).then((res) => {
+      if (res.data) {
+        let index = 0;
+        window.pixiviz.hostMap = {};
+        Object.keys(res.data).forEach((key) => {
+          window.pixiviz.hostMap[index] = key;
+          window.pixiviz.hostMap[key] = index;
+          index++;
+        });
+        window.pixiviz.proxyMap = res.data;
+      }
+    }, () => {
+      console.log('Cannot fetch proxy config.');
+    });
+  },
+  created() {
+    // 检测Safari
+    window.isSafari = /Safari/.test(navigator.userAgent) && !/Chrome/.test(navigator.userAgent);
+  },
+  mounted() {
+    // add save loadmap listener
+    window.addEventListener('beforeunload', this.saveLoadMap);
+  },
+  destroyed() {
+    window.removeEventListener('beforeunload', this.saveLoadMap);
+  },
+  methods: {
+    saveLoadMap() {
+      if (window.pixiviz.loadMap && Object.keys(window.pixiviz.loadMap).length > 0) {
+        window.localStorage.setItem('loadmap', JSON.stringify(window.pixiviz.loadMap));
+        window.localStorage.setItem('loadmap-save-time', new Date().valueOf());
+      }
+    }
+  }
 }
 </script>
 
 <style>
 .v-enter {
-    opacity: 0;
+  opacity: 0;
 }
 .v-enter-active {
-    transition: 200ms;
+  transition: 200ms;
 }
 .v-enter-to {
-    opacity: 1;
+  opacity: 1;
 }
 .v-leave {
-    opacity: 1;
+  opacity: 1;
 }
 .v-leave-to {
-    opacity: 0;
+  opacity: 0;
 }
 .v-leave-active {
-    transition: 200ms;
+  transition: 200ms;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,44 +1,46 @@
-import Vue from 'vue'
+import Vue from 'vue';
 
 // Import dependencies
-import qs from "qs"
-import VueAxios from 'vue-axios/dist/vue-axios.min'
+import qs from 'qs';
+import VueAxios from 'vue-axios/dist/vue-axios.min';
 import axios from 'axios';
 import VueCookies from 'vue-cookies';
-import VueLazyload from 'vue-lazyload'
-import Vue2TouchEvents from 'vue2-touch-events'
+import VueLazyload from 'vue-lazyload';
+import Vue2TouchEvents from 'vue2-touch-events';
 import { VueMasonryPlugin } from 'vue-masonry';
 import InfiniteLoading from 'vue-infinite-loading';
-import VueMeta from 'vue-meta'
+import VueMeta from 'vue-meta';
 
 // Import element ui and styles
 
-import './plugins/element.js'
+import './plugins/element.js';
 import './styles/main.less';
 
 // Import store
 import store from './store';
 
 // Import view and router
-import App from './App.vue'
-import router from './router'
-import './registerServiceWorker'
+import App from './App.vue';
+import router from './router';
+import './registerServiceWorker';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
 // Set up axios
-axios.defaults.baseURL = ''
-axios.defaults.withCredentials = true
-axios.defaults.timeout = 10000
-axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded'
-axios.defaults.transformRequest = [function (data) {
-  return qs.stringify(data, {
-    arrayFormat: 'brackets'
-  })
-}]
+axios.defaults.baseURL = '';
+axios.defaults.withCredentials = true;
+axios.defaults.timeout = 10000;
+axios.defaults.headers.post['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.headers.get['Content-Type'] = 'application/x-www-form-urlencoded';
+axios.defaults.transformRequest = [
+  function(data) {
+    return qs.stringify(data, {
+      arrayFormat: 'brackets',
+    });
+  },
+];
 
-Vue.use(VueAxios, axios)
+Vue.use(VueAxios, axios);
 
 // Set up lazyload
 Vue.use(VueLazyload, {
@@ -47,21 +49,21 @@ Vue.use(VueLazyload, {
   // optional
   observerOptions: {
     rootMargin: '0px',
-    threshold: 0.1
-  }
-})
+    threshold: 0.1,
+  },
+});
 
 // Mobile events
-Vue.use(Vue2TouchEvents)
+Vue.use(Vue2TouchEvents);
 
 // Set up things for waterfall
-Vue.use(VueMasonryPlugin)
+Vue.use(VueMasonryPlugin);
 Vue.use(InfiniteLoading, {
   slots: {
     noResults: '这里没有图片...',
-    noMore: '没有更多图片了...'
+    noMore: '没有更多图片了...',
   },
-})
+});
 
 // Set up vue cookies
 Vue.use(VueCookies);
@@ -72,8 +74,8 @@ Vue.use(VueMeta);
 new Vue({
   router,
   store,
-  render: h => h(App),
+  render: (h) => h(App),
   mounted() {
     document.dispatchEvent(new Event('render-event'));
-  }
+  },
 }).$mount('#app');
