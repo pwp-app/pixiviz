3a0b983ddd8bea59fee09b89e4a86dad1c0cf075
BackRunner <dev@backrunner.top>
2020-12-07 00:07:11 +0800

    fix: setvh load call

--- src/App.vue
+++ src/App.vue
@@ -77,8 +77,8 @@ export default {
         const vh = window.innerHeight * 0.01;
         document.documentElement.style.setProperty('--vh', `${vh}px`);
       };
-      window.addEventListener('load', setVh);
       window.addEventListener('resize', setVh);
+      setVh();
     }
   },
   created() {
