f8b42436586de02b5d464304869b16f756217bb0
BackRunner <dev@backrunner.top>
2020-12-16 23:00:08 +0800

    fix: check version match

--- src/views/Landing.vue
+++ src/views/Landing.vue
@@ -85,6 +85,14 @@ export default {
       this.$refs.bannerPlaceholder.expandedChanged(expanded);
     },
     // announce
+    checkVersionMatch(vers) {
+      for (const ver of vers) {
+        if (!version.startsWith(ver)) {
+          return false;
+        }
+      }
+      return true;
+    },
     fetchAnnounce() {
       this.axios.get('https://config.backrunner.top/pixiviz/announcement.json', {
         withCredentials: false,
@@ -95,10 +103,11 @@ export default {
         for (let announcement of res.data) {
           const { id, title, content, footer, expires, matchVersion } = announcement;
           const announceLog = window.localStorage.getItem('announce-read-id');
+          const versionMatched = matchVersion && this.checkVersionMatch(matchVersion);
           if (
             (announceLog && parseInt(announceLog, 10) >= parseInt(id, 10)) ||
             (dayjs(expires).unix() <= dayjs().unix()) ||
-            (matchVersion && !matchVersion.startsWith(version))
+            !versionMatched
           ) {
             continue;
           }
