7c1d5cf9f0cacbc6633151316a8be278015d41e1
BackRunner <dev@backrunner.top>
2020-12-19 01:01:04 +0800

    fix: getImageSource map could not exists

--- src/App.vue
+++ src/App.vue
@@ -119,16 +119,19 @@ export default {
       window.localStorage.setItem('loadmap-save-time', new Date().valueOf());
     },
     // 图片加载处理
-    imageLoadedHandler({ el, src }) {
-      const type = el.getAttribute('data-type');
-      if (type === 'card') {
-        this.$bus.$emit(`image-loaded-card-${el.getAttribute('data-index')}`);
-      }
+    imageLoadedHandler(e) {
+      this.emitImageCardEvent({ ...e, eventType: 'loaded' });
     },
-    imageLoadErrorHandler({ el, src }) {
+    imageLoadErrorHandler(e) {
+      this.emitImageCardEvent({ ...e, eventType: 'error' });
+    },
+    emitImageCardEvent({ el, src, eventType }) {
       const type = el.getAttribute('data-type');
       if (type === 'card') {
-        this.$bus.$emit(`image-error-card-${el.getAttribute('data-index')}`);
+        const matched = /(\d+)_p/.exec(src);
+        if (!matched || matched.length < 1) return;
+        const eventKey = `image-${eventType}-card-${el.getAttribute('data-index')}_${matched[1]}`;
+        this.$bus.$emit(eventKey);
       }
     }
   }
