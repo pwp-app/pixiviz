0451eb59b1b6eda46ef25aa8cd950a037ed02033
BackRunner <dev@backrunner.top>
2020-12-20 01:18:40 +0800

    feat: add download list

--- src/components/common/DownloadList.vue
+++ src/components/common/DownloadList.vue
@@ -1,15 +1,71 @@
 <template>
-  <div class="right-tag tag-download">
-    <span>图片下载中 ({{donwloadCount}})</span>
-  </div>
+  <el-drawer
+    class="download-list"
+    title="下载列表"
+    :visible.sync="showDrawer"
+    @before-close="handleClose"
+    >
+      <template v-if="!empty">
+        <DonwloadListItem
+          v-for="item in items"
+          :key="item.key"
+          :item="item"
+          />
+      </template>
+      <template v-else>
+        <span class="download-list-empty">当前没有进行中的下载...</span>
+      </template>
+  </el-drawer>
 </template>
 
 <script>
+import DonwloadListItem from './DownloadListItem';
+
 export default {
+  components: {
+    DonwloadListItem,
+  },
+  data() {
+    return {
+      showDrawer: true,
+      items: [],
+    };
+  },
+  created() {
+    this.$bus.$on('openDownloadList', this.show);
+  },
   computed: {
-    donwloadCount() {
-      return this.$store.state.download.list.length;
+    empty() {
+      return this.$store.state.download.list < 1;
     }
   },
+  methods: {
+    show() {
+      if (!this.showDrawer) {
+        this.showDrawer = true;
+      }
+      this.refreshList();
+      this.timer = setInterval(this.refreshList, 1000);
+    },
+    handleClose() {
+      clearInterval(timer);
+      timer = null;
+    },
+    refreshList() {
+      const list = this.$store.state.download.list;
+      if (list.length < 1) {
+        return;
+      }
+      const items = [];
+      list.forEach((item, index) => {
+        items.push({
+          key: `${item.name}_${index}`,
+          name: item.name,
+          progress: item.image.percent || 0,
+        });
+      });
+      this.items = items;
+    }
+  }
 }
 </script>
