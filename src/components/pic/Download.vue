6fe6955374d1f10c94994557d8abdb0ca34e91c0
BackRunner <dev@backrunner.top>
2021-03-06 15:59:49 +0800

    feat: download qr animation

--- src/components/pic/Download.vue
+++ src/components/pic/Download.vue
@@ -2,21 +2,39 @@
   <div class="pic-download">
     <div class="pic-download-title">
       <span>下载</span>
-      <i class="pic-download-title-icon mobile-hide icon-qrcode" @click="openDownloadQRCode">
+      <i
+        class="pic-download-title-icon mobile-hide icon-qrcode"
+        @click="openDownloadQRCode"
+      >
         <QRCodeIcon />
       </i>
-      <i class="pic-download-title-icon el-icon-setting" @click="openDownloadSettings"></i>
+      <i
+        class="pic-download-title-icon el-icon-setting"
+        @click="openDownloadSettings"
+      ></i>
     </div>
     <div class="pic-download-items">
-      <el-button type="primary" @click="downloadCurrent" :disabled="disableDownloadCurrent" :style="downloadCurrentStyle">{{ downloadCurrentText }}</el-button>
-      <el-button type="primary" @click="downloadAll" v-if="showDownloadAll" :disabled="downloadStarted">下载所有</el-button>
+      <el-button
+        type="primary"
+        @click="downloadCurrent"
+        :disabled="disableDownloadCurrent"
+        :style="downloadCurrentStyle"
+        >{{ downloadCurrentText }}</el-button
+      >
+      <el-button
+        type="primary"
+        @click="downloadAll"
+        v-if="showDownloadAll"
+        :disabled="downloadStarted"
+        >下载所有</el-button
+      >
     </div>
     <el-dialog
       class="dialog pic-download-dialog"
       title="下载设置"
       :visible.sync="settingsVisible"
       @close="handleSettingsClose"
-      >
+    >
       <el-form label-position="left" label-width="118px" :model="settingsForm">
         <el-form-item label="单张下载文件名">
           <el-input v-model="settingsForm.singleFileName"></el-input>
@@ -47,12 +65,12 @@
 </template>
 
 <script>
-import CONFIG from '../../config.json';
-import QRCodeIcon from '../icons/qrcode';
-import DownloadQRCode from './DownloadQRCode';
+import CONFIG from "../../config.json";
+import QRCodeIcon from "../icons/qrcode";
+import DownloadQRCode from "./DownloadQRCode";
 
 export default {
-  props: ['image', 'loaded'],
+  props: ["image", "loaded"],
   components: {
     QRCodeIcon,
     DownloadQRCode,
@@ -60,7 +78,7 @@ export default {
   data() {
     return {
       // download
-			downloadStarted: false,
+      downloadStarted: false,
       downloadCurrentLock: false,
       downloadCurrentStyle: null,
       // dialog
@@ -80,29 +98,33 @@ export default {
       }
     },
     downloadCurrentText() {
-      if (!this.image) return '';
-      if (!this.loaded && this.$store.state.pic.progress && this.$store.state.pic.progress < 100) {
+      if (!this.image) return "";
+      if (
+        !this.loaded &&
+        this.$store.state.pic.progress &&
+        this.$store.state.pic.progress < 100
+      ) {
         return `大图加载中（${this.$store.state.pic.progress}%）...`;
       }
-      return this.image.page_count > 1 ? '保存当前' : '点我保存';
+      return this.image.page_count > 1 ? "保存当前" : "点我保存";
     },
-		showDownloadAll() {
+    showDownloadAll() {
       if (!this.image) return false;
-			return this.image.page_count > 1;
+      return this.image.page_count > 1;
     },
     disableDownloadCurrent() {
       return this.downloadCurrentLock || !this.loaded;
-    }
+    },
   },
   watch: {
-    '$store.state.pic.progress': function(value) {
+    "$store.state.pic.progress": function (value) {
       if (this.$store.state.darkMode.enabled) {
         this.downloadCurrentStyle = `background-image: linear-gradient(to right, #d7707c 0%, #d7707c ${value}%, #999 ${value}%, #999 100%) !important;`;
       } else {
         this.downloadCurrentStyle = `background-image: linear-gradient(to right, #da7a85 0%, #da7a85 ${value}%, #999 ${value}%, #999 100%) !important;`;
       }
       this.$forceUpdate();
-    }
+    },
   },
   methods: {
     openDownloadQRCode() {
@@ -112,49 +134,54 @@ export default {
       this.settingsVisible = true;
     },
     handleSettingsClose() {
-      const keys = ['singleFileName', 'multiFileName'];
+      const keys = ["singleFileName", "multiFileName"];
       keys.forEach((key) => {
         this.settingsForm[key] = this.$downloadSettings[key];
       });
     },
     handleSettingsSubmit() {
-      const keys = ['singleFileName', 'multiFileName'];
+      const keys = ["singleFileName", "multiFileName"];
       const defaultValue = {
-        singleFileName: '{title}-{id}',
-        multiFileName: '{title}-{id}-{index}',
+        singleFileName: "{title}-{id}",
+        multiFileName: "{title}-{id}-{index}",
       };
       keys.forEach((key) => {
-        this.$downloadSettings[key] = this.settingsForm[key] || defaultValue[key];
+        this.$downloadSettings[key] =
+          this.settingsForm[key] || defaultValue[key];
       });
       // 写入localStorage
-      window.localStorage.setItem('download-settings', JSON.stringify(this.$downloadSettings));
+      window.localStorage.setItem(
+        "download-settings",
+        JSON.stringify(this.$downloadSettings)
+      );
       this.settingsVisible = false;
     },
     createDownloadTimer() {
-			window.pixiviz.downloadTimer = setInterval(() => {
-				if (window.pixiviz.downloadQueue.length) {
-					const image = window.pixiviz.downloadQueue.shift();
-					this.downloadImage(image.url, image.name);
-					window.pixiviz.downloadIdleSeconds = 0;
-				} else {
-					window.pixiviz.downloadIdleSeconds++;
-					// 闲置超过10秒即销毁
-					if (window.pixiviz.downloadIdleSeconds > 10) {
-						clearInterval(window.pixiviz.downloadTimer);
-					}
-				}
-			}, 1000);
+      window.pixiviz.downloadTimer = setInterval(() => {
+        if (window.pixiviz.downloadQueue.length) {
+          const image = window.pixiviz.downloadQueue.shift();
+          this.downloadImage(image.url, image.name);
+          window.pixiviz.downloadIdleSeconds = 0;
+        } else {
+          window.pixiviz.downloadIdleSeconds++;
+          // 闲置超过10秒即销毁
+          if (window.pixiviz.downloadIdleSeconds > 10) {
+            clearInterval(window.pixiviz.downloadTimer);
+          }
+        }
+      }, 1000);
     },
     getDownloadName(type) {
       let name;
-      if (type === 'single') {
+      if (type === "single") {
         name = this.$downloadSettings.singleFileName;
       } else {
         name = this.$downloadSettings.multiFileName;
       }
-      name = name.replace('{title}', this.image.title)
-        .replace('{id}', this.image.id)
-        .replace('{author}', this.image.user.name);
+      name = name
+        .replace("{title}", this.image.title)
+        .replace("{id}", this.image.id)
+        .replace("{author}", this.image.user.name);
       return name;
     },
     downloadImage(src, name, queue = false) {
@@ -166,65 +193,70 @@ export default {
         return;
       }
       const image = new Image();
-      image.setAttribute('crossOrigin', '*');
+      image.setAttribute("crossOrigin", "*");
       image.onload = () => {
-        const canvas = document.createElement('canvas');
+        const canvas = document.createElement("canvas");
         canvas.width = image.width;
         canvas.height = image.height;
-        const context = canvas.getContext('2d');
+        const context = canvas.getContext("2d");
         context.drawImage(image, 0, 0, image.width, image.height);
-        const url = canvas.toDataURL('image/png');
-        let a = document.createElement('a');
+        const url = canvas.toDataURL("image/png");
+        let a = document.createElement("a");
         a.download = name;
         a.href = url;
         a.click();
         a = null;
-        if (this.$store.getters['download/hasName'](name)) {
-          this.$store.commit('download/removeItem', name);
+        if (this.$store.getters["download/hasName"](name)) {
+          this.$store.commit("download/removeItem", name);
         }
       };
       setTimeout(() => {
         if (!image.blobLoaded) {
-          this.$store.commit('download/addItem', {
+          this.$store.commit("download/addItem", {
             name,
             image,
           });
         }
       }, 1000);
       image.load(src);
-		},
-		downloadCurrent() {
+    },
+    downloadCurrent() {
       this.downloadCurrentStyle = null;
-			if (!this.downloadCurrentLock) {
-				this.downloadCurrentLock = true;
-				setTimeout(() => {
-					this.downloadCurrentLock = false;
-				}, 1000);
+      if (!this.downloadCurrentLock) {
+        this.downloadCurrentLock = true;
+        setTimeout(() => {
+          this.downloadCurrentLock = false;
+        }, 1000);
       }
-			this.$emit('download-current');
-		},
+      this.$emit("download-current");
+    },
     downloadAll() {
       if (!this.originalUrls) {
-				this.$message.error('无法获取所有图片的文件地址');
+        this.$message.error("无法获取所有图片的文件地址");
         return;
       }
       // notice
       this.notification = this.$notify({
-        title: '',
-        position: 'top-right',
-        customClass: 'oneline-notice-container',
+        title: "",
+        position: "top-right",
+        customClass: "oneline-notice-container",
         dangerouslyUseHTMLString: true,
         duration: 2000,
         message: `
           <div class="oneline-notice">
             <span data-name="notice-download">您的下载已经开始了~</span>
-          </div>`
+          </div>`,
       });
-      const name = this.getDownloadName('multi');
+      const name = this.getDownloadName("multi");
       for (let i = 0; i < this.originalUrls.length; i++) {
-        const { image_urls: { original: url } } = this.originalUrls[i];
-        this.downloadImage(url.replace('i.pximg.net', CONFIG.DOWNLOAD_HOST),
-          name.replace('{index}', i), window.isSafari);
+        const {
+          image_urls: { original: url },
+        } = this.originalUrls[i];
+        this.downloadImage(
+          url.replace("i.pximg.net", CONFIG.DOWNLOAD_HOST),
+          name.replace("{index}", i),
+          window.isSafari
+        );
       }
       if (window.isSafari) {
         if (!window.downloadTimer) {
@@ -237,6 +269,6 @@ export default {
       }, 3000);
       this.contextMenuVisible = false;
     },
-  }
-}
+  },
+};
 </script>
--- src/components/pic/DownloadQRCode.vue
+++ src/components/pic/DownloadQRCode.vue
@@ -1,28 +1,31 @@
 <template>
-  <div class="downloadqr-wrapper" @click.self="handleWrapperClicked" :style="wrapperStyles">
-    <div class="downloadqr">
-      <div class="downloadqr-code" v-html="code"></div>
-      <div class="downloadqr-tip">
-        <span>请使用手机扫描二维码下载图片</span>
+  <transition name="downloadqr-fade">
+    <div
+      class="downloadqr-wrapper"
+      @click.self="handleWrapperClicked"
+      v-if="show"
+    >
+      <div class="downloadqr">
+        <div class="downloadqr-code" v-html="code"></div>
+        <div class="downloadqr-tip">
+          <span>请使用手机扫描二维码下载图片</span>
+        </div>
       </div>
     </div>
-  </div>
+  </transition>
 </template>
 
 <script>
-import QRCode from 'qrcode-svg';
+import QRCode from "qrcode-svg";
 
 export default {
   data() {
     return {
       show: false,
-      url: 'https://pixiviz.pwp.app/',
+      url: "https://pixiviz.pwp.app/",
     };
   },
   computed: {
-    wrapperStyles() {
-      return this.show ? "display: flex;" : "display: none;";
-    },
     code() {
       return new QRCode(this.url).svg();
     },
@@ -31,12 +34,12 @@ export default {
     open() {
       this.show = true;
       let url = window.location.href;
-      if (window.location.href.includes('?')) {
-        url += '&';
+      if (window.location.href.includes("?")) {
+        url += "&";
       } else {
-        url += '?';
+        url += "?";
       }
-      url += 'mobileDownload=true';
+      url += "mobileDownload=true";
       this.url = url;
     },
     handleWrapperClicked() {
