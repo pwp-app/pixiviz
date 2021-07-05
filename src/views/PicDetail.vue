3ae2ffe4816b778742f9c5c56292afe94cfc9701
BackRunner <dev@backrunner.top>
2021-07-05 23:24:12 +0800

    feat: use beforeDestroy rather than destroyed

--- src/views/PicDetail.vue
+++ src/views/PicDetail.vue
@@ -191,7 +191,7 @@ export default {
       });
     }
   },
-  destroyed() {
+  beforeDestroy() {
     window.removeEventListener('orientationchange', this.handleScreenRotate, false);
     window.removeEventListener('scroll', this.handleScroll);
   },
