45d2f96d42732663455d7a71de4afbc1a0af5409
BackRunner <dev@backrunner.top>
2021-07-06 19:22:07 +0800

    minor: bump version

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-export const version = '1.22.3';
-console.log('%cPixiviz - v1.22.3\nEnvironment - prod', 'color:#da7a85');
-_hmt.push(['_setCustomVar', 1, 'siteVersion', '1.22.3', 1]);
+export const version = '1.23.0';
+console.log('%cPixiviz - v1.23.0\nEnvironment - prod', 'color:#da7a85');
+_hmt.push(['_setCustomVar', 1, 'siteVersion', '1.23.0', 1]);
