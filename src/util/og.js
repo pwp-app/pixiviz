10a63bc10792ed19f47f8710f1a74a4ac52fed6c
BackRunner <dev@backrunner.top>
2021-07-24 20:30:59 +0800

    fix: proxy

--- src/util/og.js
+++ src/util/og.js
@@ -3,8 +3,8 @@ let ogTags;
 const createProxy = (el, fn) => {
   return new Proxy(fn, {
     apply(target, thisArg, args) {
-      el.setAttribute.call(el, args);
-      return target.call(thisArg, args);
+      el.setAttribute.call(el, ...args);
+      return target.call(thisArg, ...args);
     },
   });
 };
@@ -14,9 +14,9 @@ export const getOgTags = () => {
     return ogTags;
   }
   const descEl = document.querySelector('#og-desc');
-  descEl.setAttribute = createProxy(document.querySelector('meta-desc'), descEl.setAttribute);
+  descEl.setAttribute = createProxy(document.querySelector('#meta-desc'), descEl.setAttribute);
   const imageEl = document.querySelector('#og-image');
-  imageEl.setAttribute = createProxy(document.querySelector('meta-image'), imageEl.setAttribute);
+  imageEl.setAttribute = createProxy(document.querySelector('#meta-image'), imageEl.setAttribute);
   ogTags = {
     ogTitle: document.querySelector('#og-title'),
     ogDesc: descEl,
