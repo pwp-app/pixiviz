e9d8ca0db2fb07348910d93f89233f9ef54c0eac
BackRunner <dev@backrunner.top>
2021-07-28 00:08:34 +0800

    chore: update build time

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.23.3';
-export const buildTime = '2021-07-27 23:47:15';
-console.log('%cPixiviz - v1.23.3\nEnvironment - prod\nBuild time: 2021-07-27 23:47:15', 'color:#da7a85');
-window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.23.3 (2021-07-27 23:47:15)', 1]);
+export const buildTime = '2021-07-28 00:08:11';
+console.log('%cPixiviz - v1.23.3\nEnvironment - prod\nBuild time: 2021-07-28 00:08:11', 'color:#da7a85');
+window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.23.3 (2021-07-28 00:08:11)', 1]);
