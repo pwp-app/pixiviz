bdd41fcd2ab6d4ef6402f16a259c21e6201984ae
BackRunner <dev@backrunner.top>
2021-07-28 00:01:42 +0800

    feat: add search failed button

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.23.3';
-export const buildTime = '2021-07-26 22:34:35';
-console.log('%cPixiviz - v1.23.3\nEnvironment - prod\nBuild time: 2021-07-26 22:34:35', 'color:#da7a85');
-window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.23.3 (2021-07-26 22:34:35)', 1]);
+export const buildTime = '2021-07-27 23:47:15';
+console.log('%cPixiviz - v1.23.3\nEnvironment - prod\nBuild time: 2021-07-27 23:47:15', 'color:#da7a85');
+window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.23.3 (2021-07-27 23:47:15)', 1]);
