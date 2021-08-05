2bdded7200da27d0c4983adecf3f33546d3e45e1
BackRunner <dev@backrunner.top>
2021-08-06 00:21:33 +0800

    chore: bump version

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-export const version = '1.24.1';
-export const buildTime = '2021-08-03 23:55:25';
-console.log('%cPixiviz - v1.24.1\nEnvironment - prod\nBuild time: 2021-08-03 23:55:25', 'color:#da7a85');
-window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.24.1 (2021-08-03 23:55:25)', 1]);
+export const version = '1.24.2';
+export const buildTime = '2021-08-06 00:21:26';
+console.log('%cPixiviz - v1.24.2\nEnvironment - prod\nBuild time: 2021-08-06 00:21:26', 'color:#da7a85');
+window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.24.2 (2021-08-06 00:21:26)', 1]);
