e08c3e9d4c0c6dcb1496e5b23abb1bf7783d4c0b
BackRunner <dev@backrunner.top>
2021-08-08 18:02:13 +0800

    fix: orientation safari compatibility

--- src/version.js
+++ src/version.js
@@ -1,6 +1,6 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.24.2';
-export const buildTime = '2021-08-08 13:06:13';
-console.log('%cPixiviz - v1.24.2\nEnvironment - prod\nBuild time: 2021-08-08 13:06:13', 'color:#da7a85');
-window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.24.2 (2021-08-08 13:06:13)', 1]);
+export const buildTime = '2021-08-08 14:52:29';
+console.log('%cPixiviz - v1.24.2\nEnvironment - prod\nBuild time: 2021-08-08 14:52:29', 'color:#da7a85');
+window._hmt && window._hmt.push(['_setCustomVar', 1, 'siteVersion', '1.24.2 (2021-08-08 14:52:29)', 1]);
