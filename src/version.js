6ac679f2957a19a04cc6e52add5fbeb1ac47de0a
BackRunner <dev@backrunner.top>
2021-10-23 15:05:26 +0800

    fix: user avatar

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.25.0';
-export const buildTime = '2021-10-17 20:05:35';
-console.log('%cPixiviz - v1.25.0\nEnvironment - prod\nBuild time: 2021-10-17 20:05:35', 'color:#da7a85');
+export const buildTime = '2021-10-23 14:13:54';
+console.log('%cPixiviz - v1.25.0\nEnvironment - prod\nBuild time: 2021-10-23 14:13:54', 'color:#da7a85');
