4483fb38c21e1f89aefcc4a40919d61fdcc82f4c
BackRunner <dev@backrunner.top>
2021-11-19 22:40:45 +0800

    feat: upgrade styles

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.25.1';
-export const buildTime = '2021-11-07 17:45:51';
-console.log('%cPixiviz - v1.25.1\nEnvironment - prod\nBuild time: 2021-11-07 17:45:51', 'color:#da7a85');
+export const buildTime = '2021-11-19 22:31:15';
+console.log('%cPixiviz - v1.25.1\nEnvironment - prod\nBuild time: 2021-11-19 22:31:15', 'color:#da7a85');
