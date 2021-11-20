b09fb0ae587ae1aaa1f03d2690a442668dc4312e
BackRunner <dev@backrunner.top>
2021-11-20 23:56:41 +0800

    feat: add hk api server to config

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-export const version = '1.25.2';
-export const buildTime = '2021-11-20 18:06:38';
-console.log('%cPixiviz - v1.25.2\nEnvironment - prod\nBuild time: 2021-11-20 18:06:38', 'color:#da7a85');
+export const version = '1.25.3';
+export const buildTime = '2021-11-20 23:56:20';
+console.log('%cPixiviz - v1.25.3\nEnvironment - prod\nBuild time: 2021-11-20 23:56:20', 'color:#da7a85');
