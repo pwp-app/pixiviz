a6e30e0e9272ab066b82021de33158d1d234dfe7
BackRunner <dev@backrunner.top>
2021-12-12 22:04:20 +0800

    feat: optimize icon and link styles

--- src/components/icons/back.vue
+++ src/components/icons/back.vue
@@ -1,22 +1,5 @@
 <template>
-  <i @click="handleClick">
-    <svg
-      t="1595702429912"
-      class="icon"
-      viewBox="0 0 1024 1024"
-      version="1.1"
-      xmlns="http://www.w3.org/2000/svg"
-      p-id="10039"
-      width="200"
-      height="200"
-    >
-      <path
-        d="M671.968 912c-12.288 0-24.576-4.672-33.952-14.048L286.048 545.984c-18.752-18.72-18.752-49.12 0-67.872l351.968-352c18.752-18.752 49.12-18.752 67.872 0 18.752 18.72 18.752 49.12 0 67.872L387.872 512.032l318.016 318.016c18.752 18.752 18.752 49.12 0 67.872-9.344 9.408-21.632 14.08-33.92 14.08z"
-        p-id="10040"
-        fill="#da7a85"
-      ></path>
-    </svg>
-  </i>
+  <i class="icon icon-back" @click="handleClick"> </i>
 </template>
 
 <script>
@@ -28,3 +11,9 @@ export default {
   },
 };
 </script>
+
+<style lang="less" scoped>
+.icon-back {
+  background-image: url('~@/assets/icons/back.svg');
+}
+</style>
--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.25.4';
-export const buildTime = '2021-12-08 03:00:08';
-console.log('%cPixiviz - v1.25.4\nEnvironment - prod\nBuild time: 2021-12-08 03:00:08', 'color:#da7a85');
+export const buildTime = '2021-12-12 21:55:58';
+console.log('%cPixiviz - v1.25.4\nEnvironment - prod\nBuild time: 2021-12-12 21:55:58', 'color:#da7a85');
