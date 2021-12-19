8d93c1b4dfb7d81cc9fc60f5b2e77d0bfcfce2b8
BackRunner <dev@backrunner.top>
2021-12-19 22:57:02 +0800

    feat: update fonts

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.25.4';
-export const buildTime = '2021-12-12 21:55:58';
-console.log('%cPixiviz - v1.25.4\nEnvironment - prod\nBuild time: 2021-12-12 21:55:58', 'color:#da7a85');
+export const buildTime = '2021-12-19 22:49:46';
+console.log('%cPixiviz - v1.25.4\nEnvironment - prod\nBuild time: 2021-12-19 22:49:46', 'color:#da7a85');
