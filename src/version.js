965071de4216a43dd571fe4ad85bb82cce70ed6d
BackRunner <dev@backrunner.top>
2022-01-12 01:16:59 +0800

    feat: remove 2022 text

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.25.5';
-export const buildTime = '2022-01-01 01:18:40';
-console.log('%cPixiviz - v1.25.5\nEnvironment - prod\nBuild time: 2022-01-01 01:18:40', 'color:#da7a85');
+export const buildTime = '2022-01-01 01:37:11';
+console.log('%cPixiviz - v1.25.5\nEnvironment - prod\nBuild time: 2022-01-01 01:37:11', 'color:#da7a85');
