6f7bda28aafde97b425c94a0dbbffaac171da72e
BackRunner <dev@backrunner.top>
2022-03-22 22:16:54 +0800

    fix: wrong rotate on mobile

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.27.0';
-export const buildTime = '2022-03-11 00:39:50';
-console.log('%cPixiviz - v1.27.0\nEnvironment - prod\nBuild time: 2022-03-11 00:39:50', 'color:#da7a85');
+export const buildTime = '2022-03-22 21:58:02';
+console.log('%cPixiviz - v1.27.0\nEnvironment - prod\nBuild time: 2022-03-22 21:58:02', 'color:#da7a85');
