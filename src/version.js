7d5545c9b00e9c9cf644ea70c1635511f8c7a42a
BackRunner <dev@backrunner.top>
2022-04-04 23:33:03 +0800

    chore: upgrade pixland

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
-export const version = '1.27.1';
-export const buildTime = '2022-04-04 17:00:15';
-console.log('%cPixiviz - v1.27.1\nEnvironment - prod\nBuild time: 2022-04-04 17:00:15', 'color:#da7a85');
+export const version = '1.28.0';
+export const buildTime = '2022-04-04 23:31:34';
+console.log('%cPixiviz - v1.28.0\nEnvironment - prod\nBuild time: 2022-04-04 23:31:34', 'color:#da7a85');
