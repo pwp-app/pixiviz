8275950d6df80318cab891f33c939417ed30f98f
BackRunner <dev@backrunner.top>
2022-04-06 00:18:42 +0800

    fix: share token generate method

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.28.0';
-export const buildTime = '2022-04-05 02:14:18';
-console.log('%cPixiviz - v1.28.0\nEnvironment - prod\nBuild time: 2022-04-05 02:14:18', 'color:#da7a85');
+export const buildTime = '2022-04-06 00:16:30';
+console.log('%cPixiviz - v1.28.0\nEnvironment - prod\nBuild time: 2022-04-06 00:16:30', 'color:#da7a85');
