941f785f36121bbeff743f8f41f4d522ec626191
BackRunner <dev@backrunner.top>
2022-06-23 01:35:23 +0800

    chore: upgrade deps

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.28.1';
-export const buildTime = '2022-06-22 22:19:17';
-console.log('%cPixiviz - v1.28.1\nEnvironment - prod\nBuild time: 2022-06-22 22:19:17', 'color:#da7a85');
+export const buildTime = '2022-06-23 01:34:20';
+console.log('%cPixiviz - v1.28.1\nEnvironment - prod\nBuild time: 2022-06-23 01:34:20', 'color:#da7a85');
