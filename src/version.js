7cfe09f10b2bf46eef0ded712f7e9cac50ff5d97
BackRunner <dev@backrunner.top>
2022-08-15 03:30:47 +0800

    fix: upgrade pixland lib

--- src/version.js
+++ src/version.js
@@ -1,5 +1,5 @@
 /* eslint-disable no-undef */
 /* eslint-disable no-console */
 export const version = '1.28.1';
-export const buildTime = '2022-06-23 01:34:20';
-console.log('%cPixiviz - v1.28.1\nEnvironment - prod\nBuild time: 2022-06-23 01:34:20', 'color:#da7a85');
+export const buildTime = '2022-08-15 03:15:50';
+console.log('%cPixiviz - v1.28.1\nEnvironment - prod\nBuild time: 2022-08-15 03:15:50', 'color:#da7a85');
